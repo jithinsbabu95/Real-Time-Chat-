bc64be2e74ae8134ff7b47f70d6e02765ce3eb7f
JITHIN.S. Babu <jithinsbabu123@gmail.com>
2020-07-19 12:04:37 +0530

    server side connected with socket io

--- app.js
+++ app.js
@@ -16,6 +16,38 @@ const server = app.listen(process.env.PORT || 3000, () => {
 //initialize socket for the server
 const io = socketio(server)
 
+// io.on('connection', socket => {
+//     console.log("New user connected")
+// })
+// io.on('connection', socket => {
+//     console.log("New user connected")
+
+//     socket.username = "Anonymous"
+
+//     socket.on('change_username', data => {
+//         socket.username = data.username
+//     })
+// })
+
 io.on('connection', socket => {
     console.log("New user connected")
+
+    socket.username = "Anonymous"
+
+    socket.on('change_username', data => {
+        socket.username = data.username
+    })
+
+
+    //handle the new message event
+    socket.on('new_message', data => {
+        console.log("new message")
+        io.sockets.emit('receive_message', {message: data.message, username: socket.username})
+    })
+
+    socket.on('typing', data => {
+        socket.broadcast.emit('typing', {username: socket.username})
+    })
+
 })
+
